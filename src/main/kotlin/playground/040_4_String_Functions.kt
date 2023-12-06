922f3e32a30916d9b34c71806fc36c29ac6d431a
ratul-space <to.ratul.hosen@gmail.com>
2023-12-06 22:11:15 +0600

    style: code formatted

--- src/main/kotlin/playground/040_4_String_Functions.kt
+++ src/main/kotlin/playground/040_4_String_Functions.kt
@@ -1,63 +1,41 @@
 package playground
 
+fun main() {
 
-
-fun main(){
-
-    val myCountry : String = "Bangladesh"
+    val myCountry: String = "Bangladesh"
 //    val myDropString : String = myCountry.drop(4)
-    val myDropString : String = myCountry.dropLast(5)
+    val myDropString: String = myCountry.dropLast(5)
     println(myDropString)
 
-     val myEleAt : Char = myCountry.elementAt(6)
+    val myEleAt: Char = myCountry.elementAt(6)
     println(myEleAt)
 
-    val myStrReplace : String = myCountry.replace("Bangla","arbi")
+    val myStrReplace: String = myCountry.replace("Bangla", "arbi")
     println(myStrReplace)
 
-    val mySub1 : String = myCountry.substring(3)
+    val mySub1: String = myCountry.substring(3)
     println(mySub1)
 
-    val myName : String = "ratul"
-    val myCapName : String = myName.capitalize()
+    val myName: String = "ratul"
+    val myCapName: String = myName.capitalize()
     println(myCapName)
 
-    val myNmeRev : String = myName.reversed()
+    val myNmeRev: String = myName.reversed()
     println(myNmeRev)
 
-    val name : String? = myName.orEmpty()
+    val name: String? = myName.orEmpty()
     println(name)
 
-    val  mySecondNumber : Int = 26
+    val mySecondNumber: Int = 26
     try {
         val result = mySecondNumber / 0
         println(result)
-    }catch (ex:Exception){
+    } catch (ex: Exception) {
         ex.printStackTrace()
         println("something wrong!")
-    }finally {
+    } finally {
         println("Finally done")
     }
     println("The end")
 
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
