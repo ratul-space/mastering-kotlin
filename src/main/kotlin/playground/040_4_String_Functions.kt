28e2b9e28df6eae051a3aa728816c5994f2c1810
ratul-space <to.ratul.hosen@gmail.com>
2023-12-06 09:37:38 +0600

    refactor: update .gitignore

---
+++ src/main/kotlin/playground/040_4_String_Functions.kt
@@ -0,0 +1,27 @@
+package playground
+
+fun main(){
+
+    val myCountry : String = "Bangladesh"
+//    val myDropString : String = myCountry.drop(4)
+    val myDropString : String = myCountry.dropLast(5)
+    println(myDropString)
+     val myEleAt : Char = myCountry.elementAt(6)
+    println(myEleAt)
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
