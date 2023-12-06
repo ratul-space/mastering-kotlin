3e868aa87620da0f5c755ca91a211ff5d7c19eb5
ratul-space <to.ratul.hosen@gmail.com>
2023-12-06 15:11:33 +0600

    feat: Different types of String

--- src/main/kotlin/playground/040_4_String_Functions.kt
+++ src/main/kotlin/playground/040_4_String_Functions.kt
@@ -1,13 +1,37 @@
 package playground
 
+
+
 fun main(){
 
     val myCountry : String = "Bangladesh"
 //    val myDropString : String = myCountry.drop(4)
     val myDropString : String = myCountry.dropLast(5)
     println(myDropString)
+
      val myEleAt : Char = myCountry.elementAt(6)
     println(myEleAt)
+
+    val myStrReplace : String = myCountry.replace("Bangla","arbi")
+    println(myStrReplace)
+
+    val mySub1 : String = myCountry.substring(3)
+    println(mySub1)
+
+    val myName : String = "ratul"
+    val myCapName : String = myName.capitalize()
+    println(myCapName)
+
+    val myNmeRev : String = myName.reversed()
+    println(myNmeRev)
+
+    val name : String? = myName.orEmpty()
+
+
+
+
+
+
 
 
 
