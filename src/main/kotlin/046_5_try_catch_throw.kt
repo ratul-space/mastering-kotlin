e05f4e5f5a3585e461ad428cd145f6254594dcde
ratul-space <to.ratul.hosen@gmail.com>
2023-12-07 13:08:09 +0600

    feat: Nested try catch throw handeling

---
+++ src/main/kotlin/046_5_try_catch_throw.kt
@@ -0,0 +1,23 @@
+fun main() {
+    try {
+        sumData(2,8)
+    }catch (e: ArithmeticException) {
+        e.printStackTrace()
+    }
+  println("End ---")
+}
+
+fun sumData(i: Int, j: Int) {
+    if (i < 5 ){
+        throw ArithmeticException("verry small number")
+    }
+
+    val r = i + j
+    println(r)
+}
+class MyNewException(a: String) : Exception("My error message.."){
+
+}
+
+
+
