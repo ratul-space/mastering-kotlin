12cb0fd08264cd5fa7bf8b1f2d01bd6b618f27eb
ratul-space <to.ratul.hosen@gmail.com>
2023-12-07 22:58:10 +0600

    refactor: clean code

--- src/main/kotlin/playground/049_Range_3_DownTo_Until_Step.kt
+++ src/main/kotlin/playground/049_Range_3_DownTo_Until_Step.kt
@@ -1,8 +1,6 @@
 package playground
 
 fun main() {
-    val myRange1: CharRange = 'c'..'h'
-    val myRange2: IntRange = 1..9
 
     // printing range from large value to small value using downTo
     for (i in 8 downTo 1) {
