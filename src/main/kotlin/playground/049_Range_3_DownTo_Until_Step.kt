fcd1e91b8d41af2c8d46e907f2fb701a50ff7f2f
ratul-space <to.ratul.hosen@gmail.com>
2023-12-07 22:45:26 +0600

    feat: printing range from the variable

--- src/main/kotlin/playground/049_Range_3_DownTo_Until_Step.kt
+++ src/main/kotlin/playground/049_Range_3_DownTo_Until_Step.kt
@@ -4,7 +4,7 @@ fun main() {
     val myRange1: CharRange = 'c'..'h'
     val myRange2: IntRange = 1..9
 
-    for (j in 1..10) {
-        println(j)
+    for (i in myRange1) {
+        println(i)
     }
 }
