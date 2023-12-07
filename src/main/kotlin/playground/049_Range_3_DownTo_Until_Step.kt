b539d75e70426b6ca7b70e8fe74a6db99f9a88de
ratul-space <to.ratul.hosen@gmail.com>
2023-12-07 22:56:27 +0600

    feat: print value range using until

--- src/main/kotlin/playground/049_Range_3_DownTo_Until_Step.kt
+++ src/main/kotlin/playground/049_Range_3_DownTo_Until_Step.kt
@@ -8,4 +8,10 @@ fun main() {
     for (i in 8 downTo 1) {
         println(i)
     }
+
+    // print value range using until
+    // in case of until it will print a value less than the max value
+    for(j in 1 until   5){
+        println(j)
+    }
 }
