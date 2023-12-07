1ad4a669a305fd755866425e3ad0f06862724af0
ratul-space <to.ratul.hosen@gmail.com>
2023-12-07 23:05:01 +0600

    feat: print normal range using step

--- src/main/kotlin/playground/049_Range_3_DownTo_Until_Step.kt
+++ src/main/kotlin/playground/049_Range_3_DownTo_Until_Step.kt
@@ -4,12 +4,17 @@ fun main() {
 
     // printing range from large value to small value using downTo
     for (i in 8 downTo 1) {
-        println(i)
+        //println(i)
     }
 
     // print value range using until
     // in case of until it will print a value less than the max value
-    for(j in 1 until   5){
-        println(j)
+    for (j in 1 until 5) {
+        //println(j)
+    }
+
+    // step
+    for (k in 1..20 step 4) {
+        println(k)
     }
 }
