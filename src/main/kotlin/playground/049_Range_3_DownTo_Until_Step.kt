6700b6882e01fc44f0ca693deff3c8716afb1b9e
ratul-space <to.ratul.hosen@gmail.com>
2023-12-07 22:50:23 +0600

    docs: add comments for downTo

--- src/main/kotlin/playground/049_Range_3_DownTo_Until_Step.kt
+++ src/main/kotlin/playground/049_Range_3_DownTo_Until_Step.kt
@@ -4,6 +4,7 @@ fun main() {
     val myRange1: CharRange = 'c'..'h'
     val myRange2: IntRange = 1..9
 
+    // printing range from large value to small value using downTo
     for (i in 8 downTo 1) {
         println(i)
     }
