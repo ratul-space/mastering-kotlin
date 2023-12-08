d793b23c882e8bec34d3a1cd661d546781c5f107
ratul-space <to.ratul.hosen@gmail.com>
2023-12-08 10:38:05 +0600

    feat: using of downTo in IntProgression

--- src/main/kotlin/playground/051_Range_5_utility_funtions.kt
+++ src/main/kotlin/playground/051_Range_5_utility_funtions.kt
@@ -1,12 +1,18 @@
 package playground
 
 fun main() {
-    val myRange1: IntRange = IntRange(2,8)
-    myRange1.forEach(){
-        println(it)    }
+    val myRange1: IntRange = IntRange(2, 8)
+    myRange1.forEach() {
+        println(it)
+    }
 
     val myRange: IntRange = 2.rangeTo(9)
-    myRange.forEach(){
+    myRange.forEach() {
+        println(it)
+    }
+
+    val myRange2: IntProgression = 9.downTo(3)
+    myRange2.forEach() {
         println(it)
     }
 }
