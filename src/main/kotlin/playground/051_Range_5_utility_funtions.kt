d6fc54bac44608ce45c88cb820a1da49653ad6ea
ratul-space <to.ratul.hosen@gmail.com>
2023-12-08 10:30:07 +0600

    feat: using of IntRange

--- src/main/kotlin/playground/051_Range_5_utility_funtions.kt
+++ src/main/kotlin/playground/051_Range_5_utility_funtions.kt
@@ -1,10 +1,13 @@
 package playground
 
 fun main() {
+    val myRange1: IntRange = IntRange(2,8)
+    myRange1.forEach(){
+        println(it)    }
+
     val myRange: IntRange = 2.rangeTo(9)
     myRange.forEach(){
         println(it)
     }
 }
-
 
