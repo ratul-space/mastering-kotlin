9323276b786d9480a68f30c0fbf90681d8c46a0c
ratul-space <to.ratul.hosen@gmail.com>
2023-12-09 08:19:22 +0600

    feat: Uses of listOf with same word

--- src/main/kotlin/playground/53_Collection_part_2.kt
+++ src/main/kotlin/playground/53_Collection_part_2.kt
@@ -1,7 +1,7 @@
 package playground
 
 fun main() {
-    val myList: List<Any> = listOf<Any>("Dell", "Samsung", "Asus", 3, 4, 8)
+    val myList: List<Any> = listOf<Any>("Dell", "Samsung", "Asus", "Apple", "Dell")
     myList.forEach() {
         println(it)
     }
