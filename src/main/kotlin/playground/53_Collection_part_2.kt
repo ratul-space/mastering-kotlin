3314eefb6467ecd12bab12ab28ddfb3c7d0482ad
ratul-space <to.ratul.hosen@gmail.com>
2023-12-09 12:27:28 +0600

    feat: Uses of listIterator

--- src/main/kotlin/playground/53_Collection_part_2.kt
+++ src/main/kotlin/playground/53_Collection_part_2.kt
@@ -3,27 +3,27 @@ package playground
 fun main() {
     val myList: List<Any> = listOf<Any>("Dell", "Samsung", "Asus", "Apple", "Dell")
     val myList2: List<Any> = listOf<Any>("Dell")
-//    myList.forEach() {
-//        println(it)
-//    }
-//    val a: Boolean = myList.contains("Samsung")
-//    println(a)
-//    val ca: Boolean = myList.containsAll(myList2)
-//    println(ca)
-//    val cac: Any = myList.get(3)
-//    println(cac)
-//    val i: Int = myList.lastIndexOf("Dell")
-//    println(i)
-//    val e: Any = myList2.isEmpty()
-//    println(e)
-//    val m: List<Any> = myList.subList(2, 4)
-//    m.forEach() {
-//        println(it)
-//    }
-//    val nl: List<Any> = myList.dropLast(n = 2)
-//    myList.forEach() {
-//        println(it)
-//    }
+    myList.forEach() {
+        println(it)
+    }
+    val a: Boolean = myList.contains("Samsung")
+    println(a)
+    val ca: Boolean = myList.containsAll(myList2)
+    println(ca)
+    val cac: Any = myList.get(3)
+    println(cac)
+    val i: Int = myList.lastIndexOf("Dell")
+    println(i)
+    val e: Any = myList2.isEmpty()
+    println(e)
+    val m: List<Any> = myList.subList(2, 4)
+    m.forEach() {
+        println(it)
+    }
+    val nl: List<Any> = myList.dropLast(n = 2)
+    myList.forEach() {
+        println(it)
+    }
     val mi = myList.listIterator(2)
     while (mi.hasNext())
         println(mi.next())
