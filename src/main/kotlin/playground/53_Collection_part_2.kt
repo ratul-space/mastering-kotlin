f48c8eae33e24afa3cd55465dd605f32a2b6d08c
ratul-space <to.ratul.hosen@gmail.com>
2023-12-09 08:30:00 +0600

    feat: Uses of containsAll

--- src/main/kotlin/playground/53_Collection_part_2.kt
+++ src/main/kotlin/playground/53_Collection_part_2.kt
@@ -2,9 +2,12 @@ package playground
 
 fun main() {
     val myList: List<Any> = listOf<Any>("Dell", "Samsung", "Asus", "Apple", "Dell")
+    val myList2: List<Any> = listOf<Any>("Dell", "Samsung", "Asus")
     myList.forEach() {
         println(it)
     }
     val a: Boolean = myList.contains("Samsung")
     println(a)
+    val ca: Boolean = myList.containsAll(myList2)
+    println(ca)
 }
