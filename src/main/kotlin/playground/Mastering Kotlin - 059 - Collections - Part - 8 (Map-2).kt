e0cec036a9d9e6e2b6ee168f9001b73710a40f47
ratul-space <to.ratul.hosen@gmail.com>
2023-12-10 22:52:14 +0600

    feat: Uses of Map

---
+++ src/main/kotlin/playground/Mastering Kotlin - 059 - Collections - Part - 8 (Map-2).kt
@@ -0,0 +1,14 @@
+package playground
+
+fun main() {
+    val myMap: Map<Int, String> = mapOf(
+
+        3 to "Apple",
+        7 to "Samsung",
+        9 to "Asus"
+    )
+    myMap.forEach() { t, u ->
+        println("Key: $t - Value: $u")
+    }
+
+}
