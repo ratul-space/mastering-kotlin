fcf5643dfd04726b95e41666be05212e139ce0e4
ratul-space <to.ratul.hosen@gmail.com>
2023-12-10 12:34:52 +0600

    feat: Uses of Mutable set

--- src/main/kotlin/playground/53_Collection_part_2.kt
+++ src/main/kotlin/playground/53_Collection_part_2.kt
@@ -1,32 +1,40 @@
 package playground
 
 fun main() {
-    val myList: List<Any> = listOf<Any>("Dell", "Samsung", "Asus", "Apple", "Dell")
-    val myList2: List<Any> = listOf<Any>("Dell")
-    myList.forEach() {
+//    val myList: MutableSet<Any> = mutableListOf() Of<Any>("Dell", "Samsung", "Asus", "Apple", "Dell")
+//    val myList2: List<Any> = listOf<Any>("Dell")
+//    myList.forEach() {
+//        println(it)
+//    }
+//    val a: Boolean = myList.contains("Samsung")
+//    println(a)
+//    val ca: Boolean = myList.containsAll(myList2)
+//    println(ca)
+////    val cac: Any = myList.get(3)
+//    println(cac)
+//    val i: Int = myList.lastIndexOf("Dell")
+//    println(i)
+//    val e: Any = myList2.isEmpty()
+//    println(e)
+////    val m: List<Any> = myList.subList(2, 4)
+//    m.forEach() {
+//        println(it)
+//    }
+//    val nl: List<Any> = myList.dropLast(n = 2)
+//    myList.forEach() {
+//        println(it)
+//    }
+//    val mi = myList.setIterator(2)
+//    while (mi.hasNext())
+//        println(mi.next())
+//    println()
+
+    val mySet: MutableSet<Any> = mutableSetOf(1, 3, 5, "microsoft", "apple", "dell", "hp")
+    mySet.forEach {
         println(it)
     }
-    val a: Boolean = myList.contains("Samsung")
-    println(a)
-    val ca: Boolean = myList.containsAll(myList2)
-    println(ca)
-    val cac: Any = myList.get(3)
-    println(cac)
-    val i: Int = myList.lastIndexOf("Dell")
-    println(i)
-    val e: Any = myList2.isEmpty()
-    println(e)
-    val m: List<Any> = myList.subList(2, 4)
-    m.forEach() {
-        println(it)
-    }
-    val nl: List<Any> = myList.dropLast(n = 2)
-    myList.forEach() {
-        println(it)
-    }
-    val mi = myList.listIterator(2)
-    while (mi.hasNext())
-        println(mi.next())
+
+
 }
 
 
