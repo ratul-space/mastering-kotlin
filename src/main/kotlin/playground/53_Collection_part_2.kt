5e4ed492eb9881363248f68b807e78456707c0f9
ratul-space <to.ratul.hosen@gmail.com>
2023-12-10 12:55:32 +0600

    feat: Uses of Space

--- src/main/kotlin/playground/53_Collection_part_2.kt
+++ src/main/kotlin/playground/53_Collection_part_2.kt
@@ -30,29 +30,41 @@ fun main() {
 //    println()
 
     val mySet: MutableSet<Any> = mutableSetOf(1, 3, 5, "microsoft", "apple", "dell", "hp")
+
     val mySet2: MutableSet<Any> = mutableSetOf("samsung", 64, 83)
+
     mySet.forEach {
+
         println(it)
     }
     println("------------1")
 
     mySet.add("asus")
+
     mySet.forEach() {
+
         println(it)
     }
     println("--------------2")
 
     mySet.clear()
+
     mySet.addAll(mySet2)
+
     mySet.forEach() {
+
         println(it)
     }
     println("---------3")
 
 //    mySet.removeAll(mySet)
+
     mySet.remove("samsung")
+
     mySet.forEach() {
+
         println(it)
+
     }
 }
 
