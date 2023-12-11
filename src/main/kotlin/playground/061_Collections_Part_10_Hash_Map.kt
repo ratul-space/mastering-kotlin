0ca81e9eb3445c2438350988806ba045ceebc799
ratul-space <to.ratul.hosen@gmail.com>
2023-12-11 09:12:05 +0600

    feat: Uses of HashMap

--- src/main/kotlin/playground/061_Collections_Part_10_Hash_Map.kt
+++ src/main/kotlin/playground/061_Collections_Part_10_Hash_Map.kt
@@ -2,4 +2,19 @@ package playground
 
 fun main() {
 
+    val myHashMap: HashMap<Int, String> = hashMapOf(
+        4 to "apple",
+        5 to "banana",
+        1 to "mango",
+        3 to "sweet"
+    )
+    myHashMap.forEach { t, u ->
+        println("K: $t V: $u")
+
+    }
+
 }
+
+
+
+
