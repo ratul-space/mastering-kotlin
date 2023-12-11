869668ae201057b478ea69daf639d2f9768d7e96
ratul-space <to.ratul.hosen@gmail.com>
2023-12-11 08:26:11 +0600

    feat: added new val

--- src/main/kotlin/playground/060_Collections_Part_8.kt
+++ src/main/kotlin/playground/060_Collections_Part_8.kt
@@ -5,13 +5,28 @@ import kotlin.coroutines.ContinuationInterceptor
 fun main() {
     val myMutableMap: MutableMap<Int, String> = mutableMapOf(
 
+
         3 to "Apple",
         7 to "Samsung",
         9 to "Asus"
     )
+    val myMap: Map<Int, String> = mapOf(
+
+        2 to "Fan",
+        5 to "AC"
+    )
+
+
+
+
 
     myMutableMap[1] = "Air"
+
+//    myMutableMap.put(1, "Air")
+
     myMutableMap.forEach { t, u ->
+
         println("K: $t - V: $u")
+
     }
 }
