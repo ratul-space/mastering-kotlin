16c2d7f6794d66bde2c4b49ae355f0424a79c341
ratul-space <to.ratul.hosen@gmail.com>
2023-12-11 07:13:57 +0600

    feat: Uses of println

--- src/main/kotlin/playground/059 _Collections _Part_8.kt
+++ src/main/kotlin/playground/059 _Collections _Part_8.kt
@@ -7,13 +7,23 @@ fun main() {
         7 to "Samsung",
         9 to "Asus"
     )
-    myMap.forEach() { t, u ->
-        println("Key: $t - Value: $u")
 
+    myMap.forEach() { t, u ->
+
+        println("Key: $t - Value: $u")
     }
+    println("---------------")
+
     myMap.forEach() {
+
         println("Key: ${it.key} value: ${it.value}")
     }
+    println("---------------")
+
     for (k in myMap.keys)
+
         println("Key: $k Value: ${myMap[k]}")
+
+    println("---------------")
+
 }
