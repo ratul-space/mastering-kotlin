500eaf51ef86259f156e2de357321244f59dc0a5
ratul-space <to.ratul.hosen@gmail.com>
2023-12-12 07:40:09 +0600

    feat: Uses of ArrayList

---
+++ src/main/kotlin/playground/Final_Chaptar_062.kt
@@ -0,0 +1,11 @@
+package playground
+
+fun main() {
+    val mal: ArrayList<String> = arrayListOf(
+        "Samsung", "Dell", "Apple"
+    )
+//    mal.add("Asus")
+//    mal.forEach() {
+//        println(it)A
+//    }
+}
