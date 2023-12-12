05cf9e31e9e87fadab0e52864fbcd1cdfc46fc27
ratul-space <to.ratul.hosen@gmail.com>
2023-12-12 07:46:55 +0600

    feat: Uses of addAll

--- src/main/kotlin/playground/Final_Chaptar_062.kt
+++ src/main/kotlin/playground/Final_Chaptar_062.kt
@@ -4,7 +4,11 @@ fun main() {
     val mal: ArrayList<String> = arrayListOf(
         "Samsung", "Dell", "Apple"
     )
+    val mal2: ArrayList<String> = arrayListOf(
+        "Dell", "Hp"
+    )
     mal.add("Asus")
+    mal.addAll(mal2)
     mal.forEach() {
         println(it)
     }
